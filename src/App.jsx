3b6266bb7144fab8f3f149f073942334f8215e32
matparraga <parragamateo17@gmail.com>
2024-09-18 12:50:53 -0500

    feat: add navigation bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 
 import "./App.css";
+import TaskNavbar from "./components/TaskNavbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>Hola mundo</>;
+  return (
+    <>
+      <TaskNavbar />
+    </>
+  );
 }
 
 export default App;
